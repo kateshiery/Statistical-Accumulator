2e61214ad66d1ff53b07bab1486e50b14285b6e4
Kate Shiery Talipnao <58163306+kateshiery@users.noreply.github.com>
2021-03-03 03:30:21 +0800

    Summary

--- main.py
+++ main.py
@@ -11,12 +11,13 @@ from tkinter import *
 import pandas as pd
 import functions
 from functions import *
+import statistics as st
 
 #GUI
 prd=0
 def show_table():
-    table.grid_remove
-    try:
+        table.grid_remove
+    #try:
         prd=int(inp.get())
         min_time = 1
         max_time = 10
@@ -24,11 +25,11 @@ def show_table():
         matrix = create_matrix(entities, prd)
         #Format output
         table.grid(row=3, column=0, pady=5)
-        rows=[]
+        summary.grid(row=4, column=0, pady=5)
         i=0
         for row in matrix:
-            cols=[]
             j=0
+            cols=[]
             for col in row:
                 space="{:>8}"
                 cols.append(col)
@@ -41,9 +42,31 @@ def show_table():
                     space="{:>10}"
                 Label(table,text=space.format(cols[j]),bg=bgc).grid(row=i, column=j, padx=3)
                 j=j+1
-            i = i+1
-    except:
-        messagebox.showerror("Invalid Input", "Please input a valid simulation period (int).")
+            i = i+1 
+        print(len(matrix))
+        Label(summary,text="Summary of Statistical Accumulators" +
+                "\n1. The number of parts produced so far: " + str(matrix[len(matrix)-1][7]) +
+                "\n2. The total of the waiting times in queue so far: " + str(matrix[len(matrix)-1][9]) +
+                "\n3. The number of parts that have passed through the queue so far: " + str(matrix[len(matrix)-1][8]) +
+                "\n4. The longest time spent in queue we’ve seen so far: " + str(matrix[len(matrix)-1][10]) +
+                "\n5. The total of the time spent in the system by all parts that have departed so far: " +
+                "\n6. The longest time in system we’ve seen so far: " + str(matrix[len(matrix)-1][12]) +
+                "\n7. The area so far under the queue-length curve Q(t): " +
+                "\n8. The highest level that Q (t) has so far attained: "  +
+                "\n9. The area so far under the server-busy function B (t): " + str(matrix[len(matrix)-1][15])
+                ,bg=bgc, justify=LEFT).grid(row=0, column=0, sticky='W', padx=3)
+        Label(summary,text="Output Performance Measures" +
+                "\n1. Total production" +
+                "\n2. Average waiting time in queue: "  +
+                "\n3. Maximum waiting time in queue: " + str(matrix[len(matrix)-1][10]) +
+                "\n4. Average total time in system: " + str(round(st.fmean(column(matrix,11)),2)) +
+                "\n5. Maximum total time in system: " + str(matrix[len(matrix)-1][12]) +
+                "\n6. Time-average number of parts in queue: " +
+                "\n7. Maximum number of parts in queue: " + 
+                "\n8. Drill-press utilization: "
+                ,bg=bgc, justify=LEFT).grid(row=0, column=1, sticky='W', padx=3)
+    #except:
+        #messagebox.showerror("Invalid Input", "Please input a valid simulation period (int).")
 
 #Convert lists to string
 def listToString(s):
@@ -53,6 +76,10 @@ def listToString(s):
 #Combine functions
 def clear():
     table.grid_remove
+
+#Extract column
+def column(matrix, i):
+    return [int(row[i]) for row in matrix[1:]]
 
 #Create window
 root=Tk()
@@ -74,6 +101,7 @@ output_frame.grid(row=0, column=1, padx=10, pady=5)
 output_frame.rowconfigure(0, weight=1)
 output_frame.columnconfigure(1, weight=1)
 table = Frame(output_frame, bg=bgc)
+summary = Frame(output_frame, bg=bgc)
 
 
 #Input text box and buttons
@@ -82,7 +110,6 @@ intVar = IntVar()
 inp = Entry(input_frame)
 inp.grid(row=1, column=0)
 Button(input_frame, text='Okay', command=show_table).grid(row=2, column=0, pady=4)
-#Button(input_frame, text='Clear', command=table.grid_remove).grid(row=3, column=0, pady=4)
 
 #Labels for the output
 Label(output_frame, text="Table Simulation of the System", bg=bgc).grid(row=0,column=0, sticky='n')
